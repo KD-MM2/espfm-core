ea2969a3919f9a84e1c58e97dc4e460e7a745914
KD-MM2 <kaotd.z3@gmail.com>
2026-07-29 23:00:48 +0900

    feat(coap): handle pwm_gpio and tach_gpio in PUT /fans/{id}

--- components/f_coap/f_coap_routes.c
+++ components/f_coap/f_coap_routes.c
@@ -166,6 +166,12 @@ static void handle_fan_put(coap_resource_t *resource, coap_session_t *session,
     if (ur.has_group_id) f_fan_set_group(h->fan, (uint8_t)id, (uint8_t)ur.group_id);
     if (ur.has_inverted) f_fan_set_inverted(h->fan, (uint8_t)id, ur.inverted);
     if (ur.has_enabled) f_fan_set_enabled(h->fan, (uint8_t)id, ur.enabled);
+    /* GPIO in-place swap: tear down old LEDC/PCNT, allocate on new pins */
+    if (ur.has_pwm_gpio || ur.has_tach_gpio) {
+        uint8_t new_pwm  = ur.has_pwm_gpio  ? (uint8_t)ur.pwm_gpio  : fi.pwm_gpio;
+        uint8_t new_tach = ur.has_tach_gpio  ? (uint8_t)ur.tach_gpio : fi.tach_gpio;
+        f_fan_set_gpio(h->fan, (uint8_t)id, new_pwm, new_tach);
+    }
     save_config(h);
     f_fan_get_info(h->fan, (uint8_t)id, &fi);
     static FanInfo pb;
